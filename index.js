6d059fb62abbd645e22a13c58559f9e22a92f206
Sch <junhg0211@gmail.com>
2025-09-26 08:56:41 +0900

    svelte with express

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from 'path';
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get("/api/hello", (req, res) => {
   res.send("Hello, World!")
 })
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+  let name = "World";
+</script>
+
+<h1>Hello {name}!</h1>
+<input bind:value={name} placeholder="Type your name" />
