7395d0a210c1aea2f585bb9fcbeaf37d5e746a5c
Sch <junhg0211@gmail.com>
2025-09-26 20:53:42 +0900

    pretendard

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<div>
+<div class="container">
   <div class="footer">서클</div>
   <slot />
   <div class="navigation">
@@ -7,3 +7,9 @@
   </div>
   <div class="header">서클</div>
 </div>
+
+<style>
+  .container {
+    font-family: 'Pretendard', sans-serif;
+  }
+</style>
