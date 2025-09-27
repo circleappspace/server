238e32806e825fbe5fe949f425307be6c800ef33
Sch <junhg0211@gmail.com>
2025-09-27 13:00:28 +0900

    iconed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,29 @@
 </script>
 
 <div class="container">
-  <div class="footer">서클</div>
+  <div class="footer">
+    <img src="/circle-icon-cherry.PNG" alt="Logo" width="24" height="24" />
+  </div>
   <slot />
   <Navigation />
-  <div class="header">서클</div>
+  <div class="header">
+    <img src="/circle-icon-cherry.PNG" alt="Logo" width="24" height="24" />
+  </div>
 </div>
 
 <style>
   .container {
     font-family: 'Pretendard', sans-serif;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 16px;
+    box-sizing: border-box;
+  }
+
+  .header, .footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 0;
   }
 </style>
