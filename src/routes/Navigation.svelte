83f962aa4b03568561dd5e263dc75d4eeddeed36
Sch <junhg0211@gmail.com>
2025-09-27 12:38:36 +0900

    design nav

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,6 +1,7 @@
 <script>
   import Cookies from 'js-cookie';
   import { onMount } from 'svelte';
+  import "bootstrap-icons/font/bootstrap-icons.css";
 
   let token = Cookies.get('token');
   let myUsername;
@@ -26,13 +27,35 @@
 </script>
 
 <div class="navigation">
-  <div><a href="/">홈</a></div>
+  <div><a href="/"><i class="bi bi-house"></i> 홈</a></div>
   {#if token}
-    <div><a href="/logout">로그아웃</a></div>
-    <div><a href="/blow">불기</a></div>
-    <div><a href="/c/{myUsername}">내 서클</a></div>
+  <div><a href="/logout"><i class="bi bi-box-arrow-right"></i> 로그아웃</a></div>
+  <div><a href="/blow"><i class="bi bi-wind"></i> 불기</a></div>
+  <div><a href="/c/{myUsername}"><i class="bi bi-person-circle"></i> 서클</a></div>
   {:else}
-    <div><a href="/login">로그인</a></div>
-    <div><a href="/register">가입</a></div>
+  <div><a href="/login"><i class="bi bi-box-arrow-in-right"></i> 로그인</a></div>
+  <div><a href="/register"><i class="bi bi-person-plus"></i> 회원가입</a></div>
   {/if}
 </div>
+
+<style>
+  .navigation {
+    display: flex;
+    gap: 1rem;
+    padding: 0.5rem 1rem;
+    justify-content: center;
+  }
+  .navigation div {
+    display: flex;
+    align-items: center;
+  }
+  .navigation a {
+    text-decoration: none;
+    color: #343a40;
+    font-weight: 500;
+  }
+  .navigation a:hover {
+    color: #007bff;
+  }
+  .bi { margin-right: 0.5rem; }
+</style>
