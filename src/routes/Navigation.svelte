fe1e58bb8a9eec4e98b8da951c3996d8cf92987a
Sch <junhg0211@gmail.com>
2025-12-16 14:08:33 +0900

    Update navigation links with dynamic icons based on current path

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,21 +1,80 @@
 <script>
   import "bootstrap-icons/font/bootstrap-icons.css";
+  import { page } from '$app/stores';
 
   export let username;
+
+  $: path = $page.url.pathname;
 </script>
 
 <div class="navigation">
-  <div><a href="/"><i class="bi bi-house"></i> 홈</a></div>
-  <div><a href="/search"><i class="bi bi-search"></i> 검색</a></div>
+  <div><a href="/">
+    {#if path === '/'}
+      <i class="bi bi-house-fill"></i>
+    {:else}
+      <i class="bi bi-house"></i>
+    {/if}
+    홈
+  </a></div>
+  <div><a href="/search">
+    {#if path === '/search'}
+      <i class="bi bi-search-heart-fill"></i>
+    {:else}
+      <i class="bi bi-search-heart"></i>
+    {/if}
+    검색
+  </a></div>
   {#if username}
-  <div><a href="/notifications"><i class="bi bi-bell"></i> 알림</a></div>
-  <div><a href="/c/{username}"><i class="bi bi-person-circle"></i> 서클</a></div>
-  <div><a href="/blow"><i class="bi bi-wind"></i> 불기</a></div>
-  <div><a href="/settings"><i class="bi bi-gear"></i> 설정</a></div>
-  <div><a href="/logout"><i class="bi bi-box-arrow-right"></i> 로그아웃</a></div>
+    <div><a href="/notifications">
+      {#if path === '/notifications'}
+        <i class="bi bi-bell-fill"></i>
+      {:else}
+        <i class="bi bi-bell"></i>
+      {/if}
+      알림
+    </a></div>
+    <div><a href="/c/{username}">
+      {#if path === `/c/${username}`}
+        <i class="bi bi-file-person-fill"></i>
+      {:else}
+        <i class="bi bi-file-person"></i>
+      {/if}
+      서클
+    </a></div>
+    <div><a href="/blow">
+      {#if path === '/blow'}
+        <i class="bi bi-chat-fill"></i>
+      {:else}
+        <i class="bi bi-chat"></i>
+      {/if}
+      블로우
+    </a></div>
+    <div><a href="/settings">
+      {#if path === '/settings'}
+        <i class="bi bi-gear-fill"></i>
+      {:else}
+        <i class="bi bi-gear"></i>
+      {/if}
+      설정
+    </a></div>
+    <div><a href="/logout"><i class="bi bi-box-arrow-right"></i> 로그아웃</a></div>
   {:else}
-  <div><a href="/login"><i class="bi bi-box-arrow-in-right"></i> 로그인</a></div>
-  <div><a href="/register"><i class="bi bi-person-plus"></i> 회원가입</a></div>
+    <div><a href="/login">
+      {#if path === '/login'}
+        <i class="bi bi-door-closed-fill"></i>
+      {:else}
+        <i class="bi bi-door-closed"></i>
+      {/if}
+      로그인
+    </a></div>
+    <div><a href="/register">
+      {#if path === '/register'}
+        <i class="bi bi-pencil-fill"></i>
+      {:else}
+        <i class="bi bi-pencil"></i>
+      {/if}
+      회원가입
+    </a></div>
   {/if}
 </div>
 
